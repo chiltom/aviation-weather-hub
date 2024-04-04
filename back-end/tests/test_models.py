f2f1ad049ff5ee4adc8bda9f139e59b508294b30
Tom Childress <thomas.childress02@gmail.com>
2024-04-04 15:58:31 -0400

    updated django test suite

--- back-end/tests/test_models.py
+++ back-end/tests/test_models.py
@@ -18,3 +18,34 @@ class Test_user(TestCase):
         )
         new_user.full_clean()
         self.assertIsNotNone(new_user)
+
+    def test_002_user_with_improper_email(self):
+        try:
+            new_user = User.objects.create_user(
+                username="tom",
+                password="thomas",
+                email="tom",
+                display_name="chiltom",
+                first_name="Tom",
+                last_name="Childress"
+            )
+            new_user.full_clean()
+            self.fail()
+        except ValidationError as e:
+            self.assertTrue("email" in e.message_dict)
+
+    def test_003_user_with_improper_names(self):
+        try:
+            new_user = User.objects.create_user(
+                username="tom@tom.com",
+                password="thomas",
+                email="tom",
+                display_name="!chiltom",
+                first_name="tom",
+                last_name="childress",
+            )
+            new_user.full_clean()
+            self.fail()
+        except ValidationError as e:
+            self.assertTrue(
+                "display_name" in e.message_dict and "first_name" in e.message_dict and "last_name" in e.message_dict)
