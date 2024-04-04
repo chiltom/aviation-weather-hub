087d06ca49e7a65b011374bd36d0e72049155486
Tom Childress <thomas.childress02@gmail.com>
2024-04-04 17:40:08 -0400

    test

--- back-end/tests/test_views.py
+++ back-end/tests/test_views.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 
 
 class Test_user_crud(TestCase):
+    # Test user signup capabilities
     def test_001_user_sign_up(self):
         client = Client()
         response = client.post(
@@ -18,3 +19,27 @@ class Test_user_crud(TestCase):
         self.assertTrue(
             b'{"user":"tom@tom.com"' in response.content and b"token" in response.content
         )
+
+    # Test user login capabilities by signing one up and logging in afterwards
+    def test_002_user_log_in(self):
+        client = Client()
+        client.post(
+            reverse("signup"),
+            data={"email": "tom@tom.com", "password": "thomas",
+                  "display_name": "chiltom", "first_name": "Tom", "last_name": "Childress"},
+            content_type="application/json"
+        )
+        response = client.post(
+            reverse("login"),
+            data={"email": "tom@tom.com", "password": "thomas"},
+            content_type="application/json"
+        )
+        with self.subTest():
+            self.assertEqual(response.status_code, 200)
+        self.assertTrue(
+            b'"user":"tom@tom.com"' in response.content and b"token" in response.content
+        )
+
+    # Test getting user info after signup
+    def test_003_user_info(self):
+        
--- back-end/weather_proj/settings.py
+++ back-end/weather_proj/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'user_app',
+    'list_app',
 ]
 
 MIDDLEWARE = [
