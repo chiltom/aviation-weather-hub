b542c43d667a0e24335dcc892c7732d01e1facf8
Tom Childress <thomas.childress02@gmail.com>
2024-04-04 12:16:11 -0400

    created user model and installed app in settings

--- back-end/user_app/models.py
+++ back-end/user_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core import validators as v
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    display_name = models.CharField(max_length=50)
+    full_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField(
+        default=18, validators=[v.MinValueValidator(16), v.MaxValueValidator(100)])
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [display_name, full_name, age]
+    # locations from many-to-many relationship
+    # lists from many-to-many relationship
