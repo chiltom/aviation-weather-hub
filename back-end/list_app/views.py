f42c4fee409d0941d2ba317eedd7dc741b62e213
Tom Childress <thomas.childress02@gmail.com>
2024-04-06 19:42:24 -0400

    finished get and post views for all_lists endpoint and added tests for them

--- back-end/list_app/views.py
+++ back-end/list_app/views.py
@@ -17,3 +17,12 @@ class All_lists(TokenReq):
         lists = get_list_or_404(request.user.lists.all())
         ser_lists = ListSerializer(lists, many=True)
         return Response(ser_lists.data, status=HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data.copy()
+        data['user'] = request.user.id
+        new_list = ListSerializer(data=data)
+        if new_list.is_valid():
+            new_list.save()
+            return Response(new_list.data, status=HTTP_201_CREATED)
+        return Response(new_list.errors, status=HTTP_400_BAD_REQUEST)
--- back-end/tests/test_views.py
+++ back-end/tests/test_views.py
@@ -75,24 +75,59 @@ class Test_user_crud(APITestCase):
             self.assertEqual(len(tokens), 0)
         self.assertEqual(response.status_code, 204)
 
+class Test_list_crud(APITestCase):
+    
+    # Test post method on All_lists view
+    def test_005_all_lists_post(self):
+        answer = {
+            "id": 1,
+            "user": 1,
+            "name": "My List",
+            "tasks": [],
+            "completed": False
+        }
+        client = Client()
+        sign_up_response = client.post(
+            reverse("signup"),
+            data={"email": "odie@odie.com", "password": "odie", "display_name": "odiesturn",
+                  "first_name": "Odie", "last_name": "Childress"},
+            content_type="application/json"
+        )
+        self.client.cookies = sign_up_response.client.cookies
+        response = self.client.post(
+            reverse("all_lists"),
+            data=json.dumps({"name": "My List", "completed": False}),
+            content_type="application/json"
+        )
+        with self.subTest():
+            self.assertEqual(response.status_code, 201)
+        self.assertEqual(json.loads(response.content), answer)
+    
     # Test get method on All_lists view
-    # def test_005_all_lists_get(self):
-    #     answer = [
-    #         {
-    #             id: 1,
-    #             "name": "My List",
-    #             "tasks": [],
-    #             "completed": False
-    #         }
-    #     ]
-    #     client = Client()
-    #     sign_up_response = client.post(
-    #         reverse("signup"),
-    #         data={"email": "odie@odie.com", "password": "odie", "display_name": "odiesturn",
-    #               "first_name": "Odie", "last_name": "Childress"},
-    #         content_type="application/json"
-    #     )
-    #     self.client.cookies = sign_up_response.client.cookies
-    #     list_post_response = self.client.post(
-    #         reverse("")
-    #     )
+    def test_006_all_lists_get(self):
+        answer =[{
+            "id": 2,
+            "user": 2,
+            "name": "My List",
+            "tasks": [],
+            "completed": False
+        }]
+        client = Client()
+        sign_up_response = client.post(
+            reverse("signup"),
+            data={"email": "odie@odie.com", "password": "odie", "display_name": "odiesturn",
+                  "first_name": "Odie", "last_name": "Childress"},
+            content_type="application/json"
+        )
+        self.client.cookies = sign_up_response.client.cookies
+        list_post_response = self.client.post(
+            reverse("all_lists"),
+            data=json.dumps({"name": "My List", "completed": False}),
+            content_type="application/json"
+        )
+        response = self.client.get(
+            reverse("all_lists")
+        )
+        with self.subTest():
+            self.assertEqual(response.status_code, 200)
+        self.assertEqual(json.loads(response.content), answer)
