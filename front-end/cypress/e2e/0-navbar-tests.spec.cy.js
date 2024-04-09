da570542c69cd8b27b335593e29e85230a63f89d
Tom Childress <thomas.childress02@gmail.com>
2024-04-08 21:14:34 -0400

    created cypress ci workflow for testing on push and pull request to main

---
+++ front-end/cypress/e2e/0-navbar-tests.spec.cy.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+// Initial tests for home page
+describe("Initial render test suite for homepage", () => {
+  it("Check for homepage load on baseUrl render", () => {
+    cy.visit("/"); // Can use / because we set baseUrl as dev server port
+    cy.get("h1").should("have.text", "The Aviation Weather Hub");
+  });
+
+  it("Check for navbar brand functionality", () => {
+    cy.visit("/");
+    cy.get(".navbar-brand").should("have.text", "Weather Hub");
+    cy.get(".navbar-brand")
+      .should("have.attr", "href")
+      .and("contain", "/")
+      .then((href) => cy.visit(href));
+    cy.get(".navbar-brand").should("have.text", "Weather Hub");
+  });
+});
--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <h1>The Aviation Weather Hub</h1>
     </>
   );
 };
