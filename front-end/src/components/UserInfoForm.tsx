65c3d6b69ff5dfdc47290a8b0cf282a6c0b54b4d
Tom Childress <thomas.childress02@gmail.com>
2024-04-10 14:36:46 -0400

    added UserInfoForm component for user info page put method

---
+++ front-end/src/components/UserInfoForm.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+
+const UserInfoForm = (): ReactElement => {
+  return (
+    <>
+      <Container>
+        <h2>User Info</h2>
+      </Container>
+    </>
+  );
+};
+
+export default UserInfoForm;
