eb8ea47b41e6708591c6c9f7ac02bf7f436d5fbb
Tom Childress <thomas.childress02@gmail.com>
2024-04-13 17:56:02 -0400

    created WeatherContext provider with a custom hook that provides state for the selected icaoCode across all children within the provider

---
+++ front-end/src/hooks/weatherContext.tsx
@@ -0,0 +1,51 @@
+import {
+  createContext,
+  useContext,
+  useState,
+  ReactNode,
+  ReactElement,
+} from "react";
+
+/**
+ * Defines the type for the context, which consists of an icaoCode and a
+ * state setter for the icaoCode.
+ */
+interface WeatherContextType {
+  icaoCode: string | null;
+  setIcaoCode: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+/**
+ * A variable that holds the context with the initial value being undefined
+ * but asserts the type that the incoming hook must be.
+ */
+const WeatherContext: React.Context<WeatherContextType | undefined> =
+  createContext<WeatherContextType | undefined>(undefined);
+
+/**
+ * Creates a custom hook to use the context within the provider, allowing any
+ * component within the weather provider to use the useWeather hook.
+ */
+export const useWeather = (): WeatherContextType => {
+  const context: WeatherContextType | undefined = useContext(WeatherContext);
+  if (context === undefined) {
+    throw new Error("useWeather must be used within a WeatherProvider");
+  }
+  return context;
+};
+
+/**
+ * The provider component that will be used in other components.
+ */
+export const WeatherProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}): ReactElement => {
+  const [icaoCode, setIcaoCode] = useState<string | null>(null);
+
+  // Provides the state and updater function to all context consumers
+  const value = { icaoCode, setIcaoCode };
+
+  return (
+    <WeatherContext.Provider value={value}>{children}</WeatherContext.Provider>
+  );
+};
